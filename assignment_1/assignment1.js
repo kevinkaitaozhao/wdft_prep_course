e7cf217e95611db57d396d32e92ef010be8e3c6b
kevinkaitaozhao <zhao.kevink@gmail.com>
2018-01-03 22:30:17 -0800

    Prepcourse Assignments

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -2,16 +2,16 @@
 Exercise #1
 Create a variable named "numbers" and assign an empty array to it.
 */
-var numbers;
-console.log('Hello World!');
+var numbers = [];
 
 /*
 Exercise #2
 Using the loop provided for you below, use the array push method to insert the numbers 1 to 10 into the array.
 */
 
-for(var i=0;i<10;i++){
+for(var i=1;i<=10;i++){
     //use the array push() method here.
+    numbers.push(i);
 }
 
 
@@ -19,8 +19,8 @@ for(var i=0;i<10;i++){
 Exercise #3
 Test that you used the array push method correctly by console logging the first item in the array. Console log the last item in the array afterwards.
 */
-console.log();
-console.log();
+console.log(numbers[0]);
+console.log(numbers[9]);
 
 
 /*
@@ -28,6 +28,8 @@ Exercise #5
 Create an empty object and assign it to the variable car below.
 Give the car object a property called "colour" and assign it the value of "black". Test that your code works correctly by outputting the "colour" property to the console. You should see the value "black" printed out.
 */
-var car;
+var car = {};
+car.colour = "black";
+console.log(car.colour);
 
 
--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -1,10 +1,24 @@
 // Exercise #1
 // Write your solution below
 
+var triangle = "";
+
+for (var i = 1; i <= 7; i++) {
+    triangle += "#";
+    console.log(triangle);
+}
 
 /* 
 Write your explanation for Exercise #1 here
+The main goal here is to create 7 rows of hashtags, with each row increasing by one hashtag as
+you increase one row. To do this, I can first create 7 rows counting numerically 1-7 by setting
+up a for loop, looping from 1-7. Then, to change those numbers to represent hashtags, I created
+an empty triangle variable and additionally incremented with += "#".
 
+Originally, I had the console.log(triangle) outside the for loop which was causing problems;
+it created a single row of 7 hashtags. I later realized in order to increment from one row 
+to the next, increasing by one hashtag each row, the console output has to be included in the
+for loop.
 */
 
 
@@ -14,8 +28,23 @@ Write your explanation for Exercise #1 here
 // Exercise #2
 // Write your solution below
 
+function isEven(number) {
+
+    if (number % 2 == 0) {
+        console.log('the number is even!')
+    }
+    else {
+        console.log('the number is odd!')
+    }
+}
+
+isEven(3);
+
 
 /* 
 Write your explanation for Exercise #2 here
-
+I set up an if/else statement that shows if number is divided by 2 with no remainder, this
+means the number must be even and isEven outputs 'the number is even!'. If the number
+is divided by 2 with a remainder, this means the number must be odd and isEven function 
+outputs 'the number is odd!'.
 */
